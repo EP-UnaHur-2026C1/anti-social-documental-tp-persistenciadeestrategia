e43b9af70b6d632c5f3a2077925dca8a0930c476
sofiaagomez <sofiaxgomez97@gmail.com>
2026-06-23 21:20:33 -0300

    feat: configurar swagger

--- src/app.js
+++ src/app.js
@@ -1,23 +1,38 @@
+const path = require("path");
+
 const express = require("express");
-const dotenv = require("dotenv");
+const YAML = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
+
 const conectarDB = require("./config/db");
 
 const routerTag = require("./routes/tagRoutes");
 const routerPost = require("./routes/postRoutes");
 
-dotenv.config();
-
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
 
 app.use(express.json());
 
-app.use("/tags", routerTag);
-app.use("/posts", routerPost);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-conectarDB();
+app.use("/api/tags", routerTag);
+app.use("/api/posts", routerPost);
 
-const PORT = process.env.PORT || 3000;
+const startServer = async () => {
+  try {
+    await conectarDB();
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Servidor en http://localhost:${PORT}`);
+      console.log(`Swagger en http://localhost:${PORT}/api-docs`);
+    });
+  } catch (error) {
+    console.error("Error iniciando el servidor:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
